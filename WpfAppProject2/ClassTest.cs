f7181e3de7a882312da772bb4e96af4a9a8effd9
X zX <sonnen_mojo1@mail.ru>
2020-03-24 22:02:06 +0600

    Test class added

---
+++ WpfAppProject2/ClassTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppProject2
+{
+   public class ClassTest
+    {
+        private int a;
+        public int A { get; set; }
+    }
+}
