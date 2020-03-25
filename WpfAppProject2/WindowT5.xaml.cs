6897894579867169b1e000135ff8bd7338d0e059
X zX <sonnen_mojo1@mail.ru>
2020-03-25 21:03:31 +0600

    windows were added

---
+++ WpfAppProject2/WindowT5.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppProject2
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowT5.xaml
+    /// </summary>
+    public partial class WindowT5 : Window
+    {
+        public WindowT5()
+        {
+            InitializeComponent();
+        }
+    }
+}
